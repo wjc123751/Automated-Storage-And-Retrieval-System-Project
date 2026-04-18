cca70dba658767f06a250a7ec57cd48f938badff
wjc123751 <1600905060@qq.com>
2026-04-19 01:25:17 +0800

    feat:test1

---
+++ uesr1/WindowsFormsApp1/Forms/tipForms.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace WindowsFormsApp1.Forms
+{
+    partial class tipForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Font = new System.Drawing.Font("华文新魏", 21.78151F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTip.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblTip.Location = new System.Drawing.Point(199, 92);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(109, 36);
+            this.lblTip.TabIndex = 0;
+            this.lblTip.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tipForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 197);
+            this.Controls.Add(this.lblTip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "tipForms";
+            this.Opacity = 0.6D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "tipForms";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TipFrom_FormClosing);
+            this.Load += new System.EventHandler(this.TipFrom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
